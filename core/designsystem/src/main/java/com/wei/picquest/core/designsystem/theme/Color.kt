5b7af07541edfebb48f4d84e3ed8e4d0d0fc13e8
WeiHe <azrael8576love2008@gmail.com>
2023-12-03 04:57:22 +0800

    spotlessApply

--- core/designsystem/src/main/java/com/wei/picquest/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/wei/picquest/core/designsystem/theme/Color.kt
@@ -63,5 +63,4 @@ val md_theme_dark_surfaceTint = Color(0xFF74DAA4)
 val md_theme_dark_outlineVariant = Color(0xFF404943)
 val md_theme_dark_scrim = Color(0xFF000000)
 
-
 val seed = Color(0xFF006741)
